b836eca9077bbd1ad7b3004e8050753dd88664a6
Nicolas <nico.caill@live.fr>
2020-11-10 17:29:18 +0100

    Adding last queries

--- Backend/helpers/constants.js
+++ Backend/helpers/constants.js
@@ -20,22 +20,34 @@ const QUERY_NAMES = [
 
 const QUERIES = {
 
+    // User view
+
     RU1: {
         operation: "aggregate",
         query: [
             { 
-                $match: { 
-                    header_location: "London" 
+                "$match": { 
+                    "header_location": "London" 
                 }
             },
             {
-                $project: {
-                    entreprise: "$header_employerName",
-                    location: "$header_location",
+                "$project": {
+                    "entreprise": "$header_employerName",
+                    "location": "$header_location",
                 },
             },
         ]
     },
+
+    RU2: {
+        operation: "aggregate",
+        query: [
+            {"$unwind": { 'path': '$salary_salaries' } }, 
+            {"$match": { 'salary_salaries.salary_salaries_val_jobTitle': 'Project Manager' } },
+            {"$group": { '_id': 'salaries', 'moyenne salaires': { "$avg": "$salary_salaries.salary_salaries_val_salaryPercentileMap_payPercentile50" }}}
+        ]
+    },
+
 
     RU3: {
         operation: "find",
@@ -50,6 +62,56 @@ const QUERIES = {
             "header_location": "Paris",
             "overview_sector": "Information Technology"
         }
+    },
+
+    // Data Analyst view
+
+    RDA1: {
+        operation: "aggregate",
+        query: [
+            {"$unwind": {'path': "$salary_salaries"}}, 
+            {"$group": {'_id': {"$toInt": "$benefits_benefitRatingDecimal"},'average:': {"$avg": '$salary_salaries.salary_salaries_val_salaryPercentileMap_payPercentile50'}}}
+        ]
+    },
+
+    RDA2: {
+        operation: "aggregate",
+        query: [
+            {"$unwind": { 'path': "$salary_salaries" } }, 
+            {"$group": {
+                "_id": "$overview_sector",
+                 "Average salary": {
+                     "$avg": "$salary_salaries.salary_salaries_val_salaryPercentileMap_payPercentile50"
+                  },
+                 "Average number of comments": {
+                     "$avg": { 
+                         "$cond": { 
+                             "if": { "$isArray": "$benefits_comments" }, 
+                             "then": { "$size": "$benefits_comments" }, 
+                             "else": "0"} 
+                         }
+                     }
+                 }
+            }    
+        ]
+    },
+
+    RDA3: {
+        operation: "aggregate",
+        query: [
+            {"$unwind": {'path': "$reviews"}},
+            {"$match": {'overview_size': '10000+ employees', 'reviews.reviews_val_date': 'last week'}},
+            {"$count": 'reviews'}        
+        ]
+    },
+
+    RDA4: {
+        operation: "aggregate",
+        query: [
+            {"$group": {"_id" : {"employer": "$header_employerName", "country": "$map_country"}, 
+            "total":{"$sum" :1}}},
+            {"$project" : {"employer" : "$_id.employer", "country" : "$_id.country", "total" : "$total", "_id" : 0}}
+        ]    
     }
 
 }
